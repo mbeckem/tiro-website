0f2fb61d14431758272c4075731a9a2a96c45f62
Michael Beckemeyer <m.beckemeyer@gmx.net>
2020-08-01 12:24:57 +0200

    Boost is no longer a dependency.

--- docker-build/src/build.sh
+++ docker-build/src/build.sh
@@ -3,5 +3,5 @@ set -e
 
 source /workspace/emsdk/emsdk_env.sh > /dev/null
 cd /workspace/build
-emcmake cmake /workspace/tiro_wasm -DCMAKE_BUILD_TYPE=Release -DTIRO_WASM_BOOST=/workspace/boost_1_72_0/
+emcmake cmake /workspace/tiro_wasm -DCMAKE_BUILD_TYPE=Release
 emmake make tiro_wasm -j $(nproc)
