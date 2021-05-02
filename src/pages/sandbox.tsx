8d01723521e8db781a46a46d9f9ed641353f1a5e
Michael Beckemeyer <m.beckemeyer@gmx.net>
2021-05-02 12:14:06 +0200

    Add a small timeout to avoid screen flashing.

--- src/pages/sandbox.tsx
+++ src/pages/sandbox.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import { Classes } from "@blueprintjs/core";
 
@@ -20,18 +20,24 @@ const LazySandbox = dynamic(async () => (await import("@components/sandbox")).Sa
 
 export default function SandboxPage(): JSX.Element {
     const [isReady, setReady] = useState(false);
+    const [isTimeoutComplete, setTimeoutComplete] = useState(false);
+    useEffect(() => {
+        setTimeout(() => setTimeoutComplete(true), 200);
+    }, []);
 
+    const displaySandbox = isReady && isTimeoutComplete;
     const sandbox = (
-        <div style={{ width: "100%", height: "100%", display: isReady ? "block" : "none" }}>
+        <div style={{ width: "100%", height: "100%", visibility: displaySandbox ? "visible" : "hidden" }}>
             <LazySandbox initialSource={INITIAL_SOURCE} onReadyChanged={setReady} />
         </div>
     );
-    const loader = isReady ? null : Loader();
+    const loader = displaySandbox ? null : Loader();
+
     return (
         <Layout fullHeight>
             <SEO title="Sandbox" />
-            {sandbox}
             {loader}
+            {sandbox}
         </Layout>
     );
 }
@@ -39,7 +45,7 @@ export default function SandboxPage(): JSX.Element {
 function Loader() {
     return (
         <div
-            style={{ marginTop: "50px", textAlign: "center", width: "100%", height: "100%" }}
+            style={{ position: "absolute", marginTop: "50px", textAlign: "center", width: "100%" }}
             className={Classes.TEXT_LARGE}
         >
             Loading Sandbox...
