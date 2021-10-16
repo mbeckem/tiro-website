e72b91048e517578d9cd69fd884f193b01440e7f
Michael Beckemeyer <m.beckemeyer@gmx.net>
2021-10-16 20:04:41 +0200

    Update typescript version

--- packages/website/src/components/__tests__/AutoLink.test.tsx
+++ packages/website/src/components/__tests__/AutoLink.test.tsx
@@ -5,7 +5,11 @@ import { AutoLink } from "../AutoLink";
 // Example test
 describe("AutoLink", () => {
     it("renders an external link for full URLs", () => {
-        render(<AutoLink className="test-class" href="https://example.com/foo">Hello World</AutoLink>);
+        render(
+            <AutoLink className="test-class" href="https://example.com/foo">
+                Hello World
+            </AutoLink>
+        );
 
         const link = screen.getByRole("link", {
             name: /Hello World/i
@@ -15,7 +19,11 @@ describe("AutoLink", () => {
     });
 
     it("renders a normal for relative hrefs", () => {
-        render(<AutoLink className="test-class" href="./foo/bar">Hello World</AutoLink>);
+        render(
+            <AutoLink className="test-class" href="./foo/bar">
+                Hello World
+            </AutoLink>
+        );
 
         const link = screen.getByRole("link", {
             name: /Hello World/i
@@ -25,7 +33,11 @@ describe("AutoLink", () => {
     });
 
     it("renders a normal for anchor hrefs", () => {
-        render(<AutoLink className="test-class" href="#anchor-123">Hello World</AutoLink>);
+        render(
+            <AutoLink className="test-class" href="#anchor-123">
+                Hello World
+            </AutoLink>
+        );
 
         const link = screen.getByRole("link", {
             name: /Hello World/i
